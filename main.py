d086b4084d0beade51e5df83713baac123e27591
Tim Clausen <timclausen1004@gmail.com>
2026-06-03 19:14:38 +0200

    name and time to dictionary
    Trying to get nam but my head is smoking...

--- main.py
+++ main.py
@@ -1,14 +1,31 @@
+leaderboard = [
+    {
+        "name": "Tom",
+        "time": "1:28.456"
+    },
+    {
+        "name": "Max",
+        "time": "1:28.782"
+    }
+]
+
 def start():
     while True:
         func = input("What do you wana do? Ad name or retrieve leaderbord? adn/wlead")
-        if 
+        if func == "adn":
+            add_name = input("Add name: ")
+            leaderboard.append(fahrer)
+            add_time = input(f"Add time of {add_name}: "))
+            add_time(f{add_time}
+                             
+                             
 
 
 
 print("Welcome to TimeRank")
 print("Made by TimFloClausen")
 
-enter = input("Press enter to strat! ")
+enter = input("Press enter to start! ")
 
 if input is (" "):
     start()
